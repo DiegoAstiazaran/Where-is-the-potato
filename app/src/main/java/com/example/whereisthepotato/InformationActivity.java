0422e054e0278b1ec77356afcb2b9b47e7bae9e3
Erik Torres <erik.torres.aguilar@gmail.com>
2019-08-24 16:38:52 -0500

    updates

--- app/src/main/java/com/example/whereisthepotato/InformationActivity.java
+++ app/src/main/java/com/example/whereisthepotato/InformationActivity.java
@@ -15,7 +15,7 @@ public class InformationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
 
-        FloatingActionButton fab = findViewById(R.id.information);
+        FloatingActionButton fab = findViewById(R.id.main_game);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
