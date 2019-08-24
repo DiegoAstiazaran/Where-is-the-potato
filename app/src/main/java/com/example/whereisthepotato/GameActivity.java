ac12ee4a78d6ec91b38b66534fe7ecc806f52a32
Luis Carlos Figueroa Rodríguez <lc.figueroar26@gmail.com>
2019-08-24 18:14:50 -0500

    Navigation added and refactor of OnClick Events

--- app/src/main/java/com/example/whereisthepotato/GameActivity.java
+++ app/src/main/java/com/example/whereisthepotato/GameActivity.java
@@ -2,10 +2,12 @@ package com.example.whereisthepotato;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements  View.OnClickListener {
 
     public Button btCreateRoom;
 
@@ -16,7 +18,19 @@ public class GameActivity extends AppCompatActivity {
 
         btCreateRoom = findViewById(R.id.create_game);
 
-//        btCreateRoom.setOnClickListener(this.);
+        btCreateRoom.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.create_game:
+                Intent intent = new Intent(view.getContext(), CreateRoomActivity.class);
+                view.getContext().startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 
 
