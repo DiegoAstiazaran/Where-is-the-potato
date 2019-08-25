4f355a03f3dd8121c856e93a9763b6aa22dfbdb7
Erik Torres <erik.torres.aguilar@gmail.com>
2019-08-25 09:52:16 -0500

    create functions

--- app/src/main/java/com/example/whereisthepotato/OneGameFragment.java
+++ app/src/main/java/com/example/whereisthepotato/OneGameFragment.java
@@ -1,17 +1,33 @@
 package com.example.whereisthepotato;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Collection;
+
+import static android.content.ContentValues.TAG;
 
 public class OneGameFragment extends Fragment {
 
     private int tabPosition;
 
     private static final String TAB_POSITION = "tab_position";
+
+    private FirebaseAuth firebaseAuth;
 
     public OneGameFragment() {
     }
@@ -38,10 +54,16 @@ public class OneGameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.game_detail, container, false);
 
+        Toast toast = Toast.makeText(getContext(), "Si esta aqui", Toast.LENGTH_SHORT);
+
+
         // TODO: load details into view; get them from firebase
+
 
         return rootView;
     }
+
+
 
 }
 
--- app/src/main/java/com/example/whereisthepotato/OneGamePagerFragment.java
+++ app/src/main/java/com/example/whereisthepotato/OneGamePagerFragment.java
@@ -2,21 +2,35 @@ package com.example.whereisthepotato;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import static android.content.ContentValues.TAG;
 
 public class OneGamePagerFragment extends Fragment {
 
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
     private ViewPager mViewPager;
+    private FirebaseFirestore firestoreDB;
 
     Button allGamesButton;
 
@@ -48,6 +62,29 @@ public class OneGamePagerFragment extends Fragment {
             public void onClick(View view) {
                 Intent intent = new Intent(getContext(), GameActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        firestoreDB = FirebaseFirestore.getInstance();
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser fbuser = firebaseAuth.getCurrentUser();
+        String fbid = fbuser.getUid();
+
+        DocumentReference docRef = firestoreDB.collection("users").document(fbid);
+
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.get("games"));
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
             }
         });
 
