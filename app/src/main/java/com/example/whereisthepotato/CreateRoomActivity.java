aacd5fbd9ba07c58f545b1b7eed3de9320977169
Luis Carlos Figueroa Rodríguez <lc.figueroar26@gmail.com>
2019-08-25 00:41:36 -0500

    Private rooms can be created

--- app/src/main/java/com/example/whereisthepotato/CreateRoomActivity.java
+++ app/src/main/java/com/example/whereisthepotato/CreateRoomActivity.java
@@ -2,6 +2,7 @@ package com.example.whereisthepotato;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.text.TextUtils;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +20,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.type.Date;
 
 import org.w3c.dom.Text;
 
@@ -46,17 +49,14 @@ public class CreateRoomActivity extends AppCompatActivity implements View.OnClic
         EditText etRoomName = findViewById(R.id.room_name);
         RadioButton rbPrivate = findViewById(R.id.private_type);
         RadioButton rbPublic = findViewById(R.id.public_type);
-        String checked = rbPublic.getText().toString();
         switch (view.getId()) {
             case R.id.public_type:
                 tvPassword.setVisibility(View.INVISIBLE);
                 etPassword.setVisibility(View.INVISIBLE);
-                checked = rbPublic.getText().toString();
                 break;
             case R.id.private_type:
                 tvPassword.setVisibility(View.VISIBLE);
                 etPassword.setVisibility(View.VISIBLE);
-                checked =  rbPrivate.getText().toString();
                 break;
             case R.id.create_room:
                 if(TextUtils.isEmpty(etRoomName.getText())) {
@@ -64,17 +64,20 @@ public class CreateRoomActivity extends AppCompatActivity implements View.OnClic
                 } else if(rbPrivate.isChecked() && TextUtils.isEmpty(etPassword.getText())) {
                     Toast.makeText(this.getApplicationContext(), "A password is required for private rooms!", Toast.LENGTH_SHORT).show();
                 } else {
+                    RadioGroup rgType = findViewById(R.id.type_options);
+                    RadioButton checked = rgType.findViewById(rgType.getCheckedRadioButtonId());
                     FirebaseFirestore database = FirebaseFirestore.getInstance();
                     FirebaseAuth mAuth = FirebaseAuth.getInstance();
                     FirebaseUser currentUser = mAuth.getCurrentUser();
                     CollectionReference games = database.collection("games");
                     User user = new User(currentUser.getEmail(), currentUser.getDisplayName(),
                             null, null, null);
-                    Room room = new Room(etRoomName.getText().toString(), checked.toLowerCase(), null, null,
-                            10.0, true, 100.0, null, user, null);
+                    Room room = new Room(etRoomName.getText().toString(), checked.getText().toString().toLowerCase(),
+                            Calendar.getInstance().getTime(), null, 10.0, true,
+                            100.0, null, user, etPassword.getText().toString());
                     games.add(room);
-//                    String toastMessage = String.format(getResources().getString(R.string.welcome_message), etRoomName.getText().toString());
-//                    Toast.makeText(getApplicationContext(), toastMessage , Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(view.getContext(), GameActivity.class);
+                    view.getContext().startActivity(intent);
                 }
                 break;
             default:
--- app/src/main/java/com/example/whereisthepotato/Room.java
+++ app/src/main/java/com/example/whereisthepotato/Room.java
@@ -3,14 +3,15 @@ package com.example.whereisthepotato;
 import android.location.Location;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 
 public class Room {
     public int ID;
     public String name;
     public String type;
-    public LocalDate startTime;
-    public LocalDate endTime;
+    public Date startTime;
+    public Date endTime;
     public double rangeToSteal;
     public boolean notificationsEnabled;
     public double gameRange;
@@ -25,7 +26,7 @@ public class Room {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public Room(String name, String type, LocalDate startTime, LocalDate endTime, double rangeToSteal, boolean notificationsEnabled, double gameRange, String region, User owner, String password) {
+    public Room(String name, String type, Date startTime, Date endTime, double rangeToSteal, boolean notificationsEnabled, double gameRange, String region, User owner, String password) {
         this.name = name;
         this.type = type;
         this.startTime = startTime;
