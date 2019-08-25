d22b61351c160146df3f102c370bf47489566d3b
Erik Torres <erik.torres.aguilar@gmail.com>
2019-08-24 21:58:11 -0500

    join rooms

--- app/src/main/java/com/example/whereisthepotato/JoinRoom.java
+++ app/src/main/java/com/example/whereisthepotato/JoinRoom.java
@@ -3,6 +3,12 @@ package com.example.whereisthepotato;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class JoinRoom extends AppCompatActivity {
 
@@ -11,4 +17,21 @@ public class JoinRoom extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_room);
     }
+
+    DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference ref = database.child("rooms");
+
+    Query roomsToJoin = ref.orderByChild(room).equalTo("+923336091371");
+    roomsToJoin.addListenerForSingleValueEvent(new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                user = singleSnapshot.getValue(User.class);
+            }
+        }
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+            Log.e(TAG, "onCancelled", databaseError.toException());
+        }
+    });
 }
