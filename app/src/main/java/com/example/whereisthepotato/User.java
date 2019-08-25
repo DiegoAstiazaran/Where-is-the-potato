e48f0dc9801ee9de985a314d6c8420a5ba6148d0
Erik Torres <erik.torres.aguilar@gmail.com>
2019-08-24 21:37:43 -0500

    create user and room classes

---
+++ app/src/main/java/com/example/whereisthepotato/User.java
@@ -0,0 +1,13 @@
+package com.example.whereisthepotato;
+
+import android.location.Location;
+
+public class User {
+
+    public String email;
+    public String name;
+    public Location location;
+    public String password;
+    public Room[] rooms;
+
+}
