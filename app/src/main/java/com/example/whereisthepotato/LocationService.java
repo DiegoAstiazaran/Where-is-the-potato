655374d25015424faaed69fd4d152f3549ce366e
Diego Astiazaran <diegoaat97@gmail.com>
2020-05-22 02:05:02 -0600

    updates location service

--- app/src/main/java/com/example/whereisthepotato/LocationService.java
+++ app/src/main/java/com/example/whereisthepotato/LocationService.java
@@ -1,8 +1,11 @@
 package com.example.whereisthepotato;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -12,6 +15,8 @@ import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -26,134 +31,156 @@ import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
+import com.intentfilter.androidpermissions.PermissionManager;
+import com.intentfilter.androidpermissions.models.DeniedPermission;
+import com.intentfilter.androidpermissions.models.DeniedPermissions;
+
+import static java.util.Collections.singleton;
 
 public class LocationService extends Service {
-        private static final String TAG = "MyLocationService";
-        private LocationManager mLocationManager = null;
-        private static final int LOCATION_INTERVAL = 1000;
-        private static final float LOCATION_DISTANCE = 5f;
+    private static final String TAG = "MyLocationService";
+    private LocationManager mLocationManager = null;
+    private static final int LOCATION_INTERVAL = 0; // 1000;
+    private static final float LOCATION_DISTANCE = 0; // 5f;
 
-        LocationListener[] mLocationListeners = new LocationListener[] {
-                new LocationListener(LocationManager.GPS_PROVIDER),
-                new LocationListener(LocationManager.NETWORK_PROVIDER)
-        };
+    LocationListener[] mLocationListeners = new LocationListener[] {
+        new LocationListener(LocationManager.GPS_PROVIDER),
+        new LocationListener(LocationManager.NETWORK_PROVIDER)
+    };
 
-        @Override
-        public IBinder onBind(Intent arg0)
-        {
-            return null;
+    private void startService() {
+        initializeLocationManager();
+        try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE,
+                    mLocationListeners[1]);
+        } catch (java.lang.SecurityException ex) {
+            Log.i(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            Log.d(TAG, "network provider does not exist, " + ex.getMessage());
         }
-
-        @Override
-        public int onStartCommand(Intent intent, int flags, int startId)
-        {
-            Log.e(TAG, "onStartCommand");
-            super.onStartCommand(intent, flags, startId);
-            return START_STICKY;
+        try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE,
+                    mLocationListeners[0]);
+        } catch (java.lang.SecurityException ex) {
+            Log.i(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            Log.d(TAG, "gps provider does not exist " + ex.getMessage());
         }
+        FirebaseApp.initializeApp(this);
+        Log.i(TAG, "Service started!");
+        Toast.makeText(getApplicationContext(), "SERVICE STARTED", Toast.LENGTH_LONG).show();
+    }
 
-        @Override
-        public void onCreate()
-        {
-            initializeLocationManager();
-            try {
-                mLocationManager.requestLocationUpdates(
-                        LocationManager.NETWORK_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE,
-                        mLocationListeners[1]);
-            } catch (java.lang.SecurityException ex) {
-                Log.i(TAG, "fail to request location update, ignore", ex);
-            } catch (IllegalArgumentException ex) {
-                Log.d(TAG, "network provider does not exist, " + ex.getMessage());
+    private void getPermissions() {
+        PermissionManager permissionManager = PermissionManager.getInstance(getApplicationContext());
+        permissionManager.checkPermissions(singleton(Manifest.permission.ACCESS_FINE_LOCATION), new PermissionManager.PermissionRequestListener() {
+            @Override
+            public void onPermissionGranted() {
+                Log.i(TAG, "Permissions were granted!");
+                startService();
             }
-            try {
-                mLocationManager.requestLocationUpdates(
-                        LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE,
-                        mLocationListeners[0]);
-            } catch (java.lang.SecurityException ex) {
-                Log.i(TAG, "fail to request location update, ignore", ex);
-            } catch (IllegalArgumentException ex) {
-                Log.d(TAG, "gps provider does not exist " + ex.getMessage());
-            }
-            FirebaseApp.initializeApp(this);
-            Toast.makeText(getApplicationContext(), "SERVICE STARTED", Toast.LENGTH_LONG).show();
-        }
 
-        @Override
-        public void onDestroy()
-        {
-            Log.e(TAG, "onDestroy");
-            super.onDestroy();
-            if (mLocationManager != null) {
-                for (int i = 0; i < mLocationListeners.length; i++) {
-                    try {
-                        mLocationManager.removeUpdates(mLocationListeners[i]);
-                    } catch (Exception ex) {
-                        Log.i(TAG, "fail to remove location listners, ignore", ex);
-                    }
+            @Override
+            public void onPermissionDenied(DeniedPermissions deniedPermissions) {
+                Log.i(TAG, "Permissions were denied!");
+                Toast.makeText(getApplicationContext(), "Permissions Denied", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "onStartCommand");
+        do {
+            getPermissions();
+            Log.i(TAG, "permissionsNotGranted");
+        } while (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED);
+        Log.i(TAG, "permissionsGranted");
+        super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.e(TAG, "onDestroy");
+        super.onDestroy();
+        if (mLocationManager != null) {
+            for (int i = 0; i < mLocationListeners.length; i++) {
+                try {
+                    mLocationManager.removeUpdates(mLocationListeners[i]);
+                } catch (Exception ex) {
+                    Log.i(TAG, "fail to remove location listeners, ignore", ex);
                 }
             }
         }
+    }
 
-        private void initializeLocationManager() {
-        Log.e(TAG, "initializeLocationManager");
+    private void initializeLocationManager() {
+        Log.i(TAG, "initializeLocationManager");
         if (mLocationManager == null) {
             mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
         }
     }
 
-        private class LocationListener implements android.location.LocationListener
-        {
-            Location mLastLocation;
+    private class LocationListener implements android.location.LocationListener {
+        Location mLastLocation;
 
-            public LocationListener(String provider)
-            {
-                Log.e(TAG, "LocationListener " + provider);
-                mLastLocation = new Location(provider);
-            }
-
-            @Override
-            public void onLocationChanged(final Location location)
-            {
-
-                final FirebaseFirestore firestoreDB = FirebaseFirestore.getInstance();
-
-                Log.e(TAG, "onLocationChanged: " + location);
-                mLastLocation.set(location);
-
-                final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-                final FirebaseUser fbuser = firebaseAuth.getCurrentUser();
-                final String fbid = fbuser.getUid();
-
-                firestoreDB.collection("users")
-                        .whereEqualTo("firebase_uid", fbid)
-                        .get()
-                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    DocumentReference userUpdate = firestoreDB.collection("users").document(fbid);
-                                    userUpdate.update("location", new GeoPoint(location.getLatitude(), location.getLongitude()));
-                                }
-                            }
-                        });
-            }
-
-            @Override
-            public void onProviderDisabled(String provider)
-            {
-                Log.e(TAG, "onProviderDisabled: " + provider);
-            }
-
-            @Override
-            public void onProviderEnabled(String provider)
-            {
-                Log.e(TAG, "onProviderEnabled: " + provider);
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras)
-            {
-                Log.e(TAG, "onStatusChanged: " + provider);
-            }
+        public LocationListener(String provider) {
+            Log.i(TAG, "LocationListener " + provider);
+            mLastLocation = new Location(provider);
         }
+
+        @Override
+        public void onLocationChanged(final Location location) {
+
+            final FirebaseFirestore firestoreDB = FirebaseFirestore.getInstance();
+
+            Log.i(TAG, "onLocationChanged: " + location);
+            mLastLocation.set(location);
+
+            final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+            final FirebaseUser fbuser = firebaseAuth.getCurrentUser();
+            final String fbid = fbuser.getUid();
+
+            firestoreDB.collection("users")
+                    .whereEqualTo("firebase_uid", fbid)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentReference userUpdate = firestoreDB.collection("users").document(fbid);
+                                userUpdate.update("location", new GeoPoint(location.getLatitude(), location.getLongitude()));
+                            }
+                        }
+                    });
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.i(TAG, "onProviderDisabled: " + provider);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.i(TAG, "onProviderEnabled: " + provider);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.e(TAG, "onStatusChanged: " + provider);
+        }
+    }
 }
